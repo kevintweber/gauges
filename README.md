f1c8d38e6cc1880e5db8b164bb6422ffe4565e47
Kevin Weber <kevin.weber@site-kitchen.com>
2014-04-25 19:48:27 -0400

    Working on Request documentation and functions.

--- README.md
+++ README.md
@@ -8,6 +8,28 @@ PHP Wrapper for the Gauges API
 Documentation
 =============
 
+Installation
+------------
+
+The recommended way to install this library is using
+[Composer] (http://getcomposer.org).
+
+Install composer and update your project's composer.json file to
+include this library:
+
+```javascript
+{
+    "require": {
+        "kevintweber/gauges": "~0.1"
+    }
+}
+```
+
+Usage
+----
+
+Consuming the Gauges API with this library is super simple.
+
 (todo)
 
 About
--- src/Gauges/Request.php
+++ src/Gauges/Request.php
@@ -5,8 +5,13 @@ namespace kevintweber\Gauges;
 use GuzzleHttp\Client;
 use Psr\Log\LoggerInterface;
 
+/**
+ * Used to make Gauges API calls.
+ */
 class Request
 {
+    const URL = 'https://secure.gaug.es';
+
     /** @var array */
     protected $httpDefaults;
 
@@ -18,6 +23,10 @@ class Request
 
     /**
      * Constructor
+     *
+     * @param string          $token        Your API token
+     * @param array           $httpDefaults See Guzzle documentation (proxy, etc.)
+     * @param LoggerInterface $logger       (Optional) A logging service.
      */
     public function __construct($token,
                                 array $httpDefaults = array(),
@@ -28,11 +37,28 @@ class Request
         $this->token = $token;
     }
 
+    /**
+     * Get Your Information
+     *
+     * Returns your information.
+     *
+     * @return GuzzleHttp\Message\Response
+     */
     public function me()
     {
-        return $this->makeApiCall('get', 'me');
+        return $this->makeApiCall(__FUNCTION__, 'GET', 'me');
     }
 
+    /**
+     * Update Your Information
+     *
+     * Updates and returns your information with the updates applied.
+     *
+     * @param string $first_name Your first name.
+     * @param string $last_name  Your last name.
+     *
+     * @return GuzzleHttp\Message\Response
+     */
     public function update_me($first_name = null, $last_name = null)
     {
         $params = array();
@@ -45,14 +71,31 @@ class Request
             $params['last_name'] = (string) $last_name;
         }
 
-        return $this->makeApiCall('put', 'me', $params);
+        return $this->makeApiCall(__FUNCTION__, 'PUT', 'me', $params);
     }
 
+    /**
+     * API Client List
+     *
+     * Returns an array of your API clients.
+     *
+     * @return GuzzleHttp\Message\Response
+     */
     public function list_clients()
     {
-        return $this->makeApiCall('get', 'clients');
+        return $this->makeApiCall(__FUNCTION__, 'GET', 'clients');
     }
 
+    /**
+     * Creating an API Client
+     *
+     * Creates an API client, which can be used to authenticate against
+     * the Gaug.es API.
+     *
+     * @param string $description Short description for the key
+     *
+     * @return GuzzleHttp\Message\Response
+     */
     public function create_client($description = null)
     {
         $params = array();
@@ -61,10 +104,134 @@ class Request
             $params['description'] = (string) $description;
         }
 
-        return $this->makeApiCall('post', 'clients', $params);
+        return $this->makeApiCall(__FUNCTION__, 'POST', 'clients', $params);
     }
 
-    protected function makeApiCall($method, $path, array $params = array())
+    /**
+     * Delete an API Client
+     *
+     * Permanently deletes an API client key.
+     *
+     * @param string $id
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function delete_client($id)
+    {
+        return $this->makeApiCall(__FUNCTION__, 'DELETE', 'clients/' . $id);
+    }
+
+    /**
+     * Gauges List
+     *
+     * Returns an array of your gauges, with recent traffic included.
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function list_gauges($page = null)
+    {
+        $params = array();
+
+        if (isset($page)) {
+            $params['page'] = (int) $page;
+        }
+
+        return $this->makeApiCall(__FUNCTION__, 'GET', 'gauges', $params);
+    }
+
+    /**
+     * Create a New Gauge
+     *
+     * Creates a gauge.
+     *
+     * @param string $title
+     * @param string $tz
+     * @param string $allowedHosts (Optional)
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function create_gauge($title, $tz, $allowedHosts = null)
+    {
+        $params = array(
+            'title' => $title,
+            'tz' => $tz
+        );
+
+        if (isset($allowedHosts)) {
+            $params['allowed_hosts'] = (string) $allowedHosts;
+        }
+
+        return $this->makeApiCall(__FUNCTION__, 'POST', 'gauges', $params);
+    }
+
+    /**
+     * Gauge Detail
+     *
+     * Gets details for a gauge.
+     *
+     * @param string $id
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function gauge_detail($id)
+    {
+        return $this->makeApiCall(__FUNCTION__, 'GET', 'gauges/' . $id);
+    }
+
+    /**
+     * Update a Gauge
+     *
+     * Updates and returns a gauge with the updates applied.
+     *
+     * @param string $id
+     * @param string $title
+     * @param string $tz
+     * @param string $allowedHosts (Optional)
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function update_gauge($id, $title, $tz, $allowedHosts = null)
+    {
+        $params = array(
+            'title' => $title,
+            'tz' => $tz
+        );
+
+        if (isset($allowedHosts)) {
+            $params['allowed_hosts'] = (string) $allowedHosts;
+        }
+
+        return $this->makeApiCall(__FUNCTION__, 'PUT', 'gauges/' . $id, $params);
+    }
+
+    /**
+     * Delete a Gauge
+     *
+     * Permanently deletes a gauge.
+     *
+     * @param string $id
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function delete_gauge($id)
+    {
+        return $this->makeApiCall(__FUNCTION__, 'DELETE', 'gauges/' . $id);
+    }
+
+    /**
+     * Make the actual gauges API call.
+     *
+     * @param string $functionName The calling function name.
+     * @param string $method       [GET|POST|PUT|DELETE]
+     * @param string $path
+     * @param array  $params
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    protected function makeApiCall($functionName,
+                                   $method,
+                                   $path,
+                                   array $params = array())
     {
         // Validate method.
         $method = strtoupper($method);
@@ -83,7 +250,7 @@ class Request
         // Make API call.
         $client = new Client(
             array(
-                'base_url' => array('https://secure.gaug.es'),
+                'base_url' => array(self::URL),
                 'defaults' => $this->httpDefaults
             )
         );
@@ -93,12 +260,23 @@ class Request
             $path,
             array('headers' => array('X-Gauges-Token' => $this->token))
         );
+        $request->setQuery($params);
 
         $response = $client->send($request);
 
-        /// @todo
+        // Log the message (if the logger is present).
         if ($this->logger) {
-            $this->logger->debug('');
+            if ($response->getStatusCode() == 200) {
+                $message = 'successful.';
+            } else {
+                $message = 'unsuccessful. (status=' . $response->getStatusCode() .
+                    ')'
+            }
+
+            $this->logger->debug('Gauges (' . self::URL . '): ' . $functionName .
+                                 ' request ' . $message);
         }
+
+        return $response;
     }
 }
